f28d06de5e70e77f263f64f4efd33329f4714581
Mace <maceplus@gmail.com>
2018-03-06 13:08:16 +0000

    add js for img

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './style.css';
-
+import Icon from './favicon.jpg';
   function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+      var myIcon = new Image();
+       myIcon.src = Icon;
+    
+       element.appendChild(myIcon);
 
     return element;
   }
