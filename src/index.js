e1b56004c8f3bae284d6a91a7937a3bdcc40d6e6
Mace <maceplus@gmail.com>
2018-03-31 02:07:42 +0100

    add new refresh button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import Icon from './favicon.jpg';
 
-// https://developers.coinbase.com/api/v2#exchange-rates
-
 fetch(getExchangeRateURL('BTC'))
   .then((response)=> {
     if(response.ok) {
@@ -17,15 +15,90 @@ fetch(getExchangeRateURL('BTC'))
   })
   .then((rates) => {
     console.log(rates)
+    return rates
+    
   })
+  .then(createTable)
+
   .catch((error)=> {
-    console.error(error)
+    console.error(error,'OOOOPS something went wrong!!')
   })
 
+  //Table Data
+  // =========================================================
+  function createTable(rates) {
+    const div1 = document.createElement("DIV");
+    div1.id = "currencyTable";
+
+    const tableData = document.createElement("TABLE" );
+      for(let i = 0; i < rates.length; i++){
+         let trow = tableData.insertRow();
+         let cell1 = trow.insertCell(0) // calling with an argument = index 0
+         let cell2 = trow.insertCell(1)
+         cell1.textContent = rates[i].curr
+         cell2.textContent = rates[i].price
+      }
+
+    tableData.style.width  = '100px' ;
+    tableData.style.border = '1px solid black';
+
+    div1.appendChild(tableData)
+    document.body.appendChild(div1);
+  }
+
+//===============================================================
+
+//New currency button
+//===============================================================
+
+function createButton() {
+const div2 = document.createElement("DIV");
+div2.id = "currencyButton";
+
+const currencyRefresh = document.createElement("INPUT");
+currencyRefresh.setAttribute("value", " Enter Currency Code");
+currencyRefresh.setAttribute("onfocus", "this.value=''");
+currencyRefresh.addEventListener ("click", () => {
+  //do somethin here
+  alert("wow");
+});
+div2.appendChild(currencyRefresh)
+
+
+document.body.appendChild(div2);
+
+}
+createButton()
+//===============================================================
+
+//Refresh button
+//===============================================================
+
+function pageRefresh() {
+  const div3 = document.createElement("DIV");
+  div3.id = "refreshButton";
+  
+  const refreshButton = document.createElement("BUTTON");
+  refreshButton.setAttribute("value", "Refresh Page");
+  refreshButton.addEventListener ("click", () => {
+    onclick = window.location.href=window.location.href
+    alert("You have refreshed for the latest exchange rate!");
+  });
+  div3.appendChild(refreshButton)
+  
+  
+  document.body.appendChild(div3);
+  
+  }
+  pageRefresh()
+
+
+//===============================================================
+
+
   function getRatesByCurrency(exchangeData, currencies) {
-    return currencies.map((code) => ({
-      code,
-      value: exchangeData[code]
+    return currencies.map((curr) => ({
+      curr, price: exchangeData[curr]
     }));
   }
   
