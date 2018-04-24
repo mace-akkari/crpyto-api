baf2b4aef9898fbd3ac096a470ab405db6a17aa2
Mace <maceplus@gmail.com>
2018-04-24 18:49:31 +0100

    refactored code & async await added

--- src/index.js
+++ src/index.js
@@ -1,59 +1,52 @@
 import './style.css';
 import Icon from './favicon.jpg';
 
-function fetchAPI(coin) {
-fetch(getExchangeRateURL()+coin)
-  .then((response)=> {
-    if(response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Bad Response')
-    }
-  })
-  .then(getRates)
-  .then((data) => {
-    return getRatesByCurrency(data, ['EUR', 'USD', 'GBP'])
-  })
-  .then((rates) => {
-    console.log(rates)
-    return rates
-    
-  })
-  .then(createTable)
+async function fetchAPI(coin) {
+  const response = await fetch(getExchangeRateURL() + coin);
 
-  .catch((error)=> {
-    console.error(error,'OOOOPS something went wrong!!')
-  })
-}
-  // =========================================================
-  //Table Data
-  // =========================================================
-  function createTable(rates) {
-
-    const originalDiv = document.querySelector("#currencyTable");
-    const newDiv = document.createElement("DIV");
-    newDiv.id = "currencyTable";
-
-    if(originalDiv !== null) {
-      document.body.removeChild(originalDiv);
-    }
-
-    const tableData = document.createElement("TABLE" );
-    
-      for(let i = 0; i < rates.length; i++){
-         let trow = tableData.insertRow();
-         let cell1 = trow.insertCell(0) // calling with an argument = index 0
-         let cell2 = trow.insertCell(1)
-         cell1.textContent = rates[i].curr
-         cell2.textContent = rates[i].price
-      }
-
-    tableData.style.width  = '100px';
-    tableData.style.border = '1px solid black';
-
-    newDiv.appendChild(tableData);
-    document.body.appendChild(newDiv);
+  if (!response.ok) {
+    throw new Error('Bad Response');
   }
+
+  try {
+    const exchangeData = await response.json();
+    const rates = getRates(exchangeData);
+    const currencyRates = getRatesByCurrency(rates, ['EUR', 'USD', 'GBP']);
+    createTable(currencyRates);
+  } catch (e) {
+    console.error(e, 'OOOOPS something went wrong!!');
+  }
+  return;
+}
+// =========================================================
+//Table Data
+// =========================================================
+function createTable(rates) {
+
+  const originalDiv = document.querySelector("#currencyTable");
+  const newDiv = document.createElement("DIV");
+  newDiv.id = "currencyTable";
+
+  if (originalDiv !== null) {
+    document.body.removeChild(originalDiv);
+  }
+
+  const tableData = document.createElement("TABLE");
+
+  for (let i = 0; i < rates.length; i++) {
+    let trow = tableData.insertRow();
+    let cell1 = trow.insertCell(0) // calling with an argument = index 0
+    let cell2 = trow.insertCell(1)
+    cell1.textContent = rates[i].curr
+    cell2.textContent = rates[i].price
+  }
+
+  tableData.style.width = '100px';
+  tableData.style.border = '1px solid black';
+
+  newDiv.appendChild(tableData);
+  document.body.appendChild(newDiv);
+}
 
 //===============================================================
 //New currency buttons
@@ -63,14 +56,29 @@ let currencyDiv = document.createElement("DIV");
 currencyDiv.id = "currencyDiv";
 document.body.appendChild(currencyDiv);
 
+function changeButton(target, buttonClass) {
+  const activeClass = "active";
+  document.querySelectorAll(`.${buttonClass}`).forEach((el) => {
+    el.classList.remove(activeClass);
+  });
+  target.classList.add(activeClass);
+}
+
 function createCurrencyButton(id, label, code) {
   const button = document.createElement("BUTTON");
-  button.id = id; 
-  button.textContent = label; 
-  button.classList.add("buttonClass", "active")
-  button.addEventListener("click", () => {
-    fetchAPI(code)
-  });
+  const buttonClass = "buttonClass";
+
+  button.id = id;
+  button.textContent = label;
+  button.classList.add(buttonClass);
+
+  const eventHandler = async (e) => {
+    const currentTarget = e.currentTarget;
+    await fetchAPI(code);
+    changeButton(currentTarget, buttonClass);
+  };
+
+  button.addEventListener("click", eventHandler);
   currencyDiv.appendChild(button)
 };
 
@@ -81,19 +89,19 @@ createCurrencyButton("ethButton", "Ethereum", "ETH");
 //===============================================================
 
 
-  function getRatesByCurrency(exchangeData, currencies) {
-    return currencies.map((curr) => ({
-      curr, price: exchangeData[curr]
-    }));
-  }
-  
-  function getRates(exchangeData) {
-    return exchangeData.data.rates;
-  }
+function getRatesByCurrency(exchangeData, currencies) {
+  return currencies.map((curr) => ({
+    curr, price: exchangeData[curr]
+  }));
+}
 
-  function getExchangeRateURL() {
-    const baseUrl = 'https://api.coinbase.com/v2/exchange-rates?currency='
-    return baseUrl 
-  }
-  // INIT 
-  fetchAPI('BTC');
+function getRates(exchangeData) {
+  return exchangeData.data.rates;
+}
+
+function getExchangeRateURL() {
+  const baseUrl = 'https://api.coinbase.com/v2/exchange-rates?currency='
+  return baseUrl
+}
+// INIT 
+fetchAPI('BTC');
