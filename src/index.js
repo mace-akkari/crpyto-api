e76efdce1fd5c326fbdad0af4b202a9ba6a158e3
Mace <maceplus@gmail.com>
2019-10-07 23:59:58 +0100

    delete logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './style.css';
-import Icon from './favicon.jpg';
 
 async function fetchAPI(coin) {
   const response = await fetch(getExchangeRateURL() + coin);
@@ -64,7 +63,7 @@ function changeButton(target, buttonClass) {
   target.classList.add(activeClass);
 }
 
-function createCurrencyButton(id, label, code, logo) {
+function createCurrencyButton(id, label, code) {
   const button = document.createElement("BUTTON");
   const buttonClass = "buttonClass";
 
@@ -84,7 +83,7 @@ function createCurrencyButton(id, label, code, logo) {
   
 };
 
-createCurrencyButton("btcButton", "BitCoin", "BTC", "src = '../images/bitCoin.png'");
+createCurrencyButton("btcButton", "BitCoin", "BTC");
 createCurrencyButton("ethButton", "Ethereum", "ETH");
 createCurrencyButton("ltcButton", "LiteCoin", "LTC");
 
